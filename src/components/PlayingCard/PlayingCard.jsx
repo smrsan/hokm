3ed3d4a6605af554a5cd44e2c81f52325e570f2b
SMRSAN <5180976-smrsan1@users.noreply.gitlab.com>
2024-09-15 20:21:50 +0330

    Fix JSDoc in PlayingCard

--- src/components/PlayingCard/PlayingCard.jsx
+++ src/components/PlayingCard/PlayingCard.jsx
@@ -9,57 +9,62 @@ import clsx from "clsx";
  * @property {{ root: string, img: string }} classes
  */
 
-/**
- * PlayingCard
- * @param {import("@mui/material").BoxProps & PlayingCardProps} props
- * @returns {JSXElement}
- */
-const PlayingCard = forwardRef(function PlayingCard(
-    { cardNum = 0, ...props },
-    ref
-) {
-    /** @type {import("react").MutableRefObject<HTMLImageElement>} */
-    const imgRef = useRef();
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [hasError, setHasError] = useState(false);
+const PlayingCard = forwardRef(
+    /**
+     * PlayingCard
+     * @param {import("@mui/material").BoxProps & PlayingCardProps} props
+     * @returns {JSXElement}
+     */
+    function PlayingCard({ cardNum = 0, ...props }, ref) {
+        /** @type {import("react").MutableRefObject<HTMLImageElement>} */
+        const imgRef = useRef();
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [hasError, setHasError] = useState(false);
 
-    const handleImageLoad = () => {
-        setIsLoaded(true);
-        setHasError(false);
-    };
+        const handleImageLoad = () => {
+            setIsLoaded(true);
+            setHasError(false);
+        };
 
-    const handleImageError = () => {
-        setIsLoaded(false);
-        setHasError(true);
-    };
+        const handleImageError = () => {
+            setIsLoaded(false);
+            setHasError(true);
+        };
 
-    return (
-        <Box
-            {...props}
-            ref={ref}
-            sx={{
-                ...playingCardRootSx,
-                ...props.sx,
-            }}
-            className={clsx(
-                playingCardClasses.root,
-                props.classes?.root,
-                props.className,
-                { [playingCardClasses.back]: cardNum === -1 }
-            )}
-        >
-            <img
-                ref={imgRef}
-                className={clsx(playingCardClasses.img, props.classes?.img, {
-                    [playingCardClasses["img-loaded"]]: isLoaded,
-                    [playingCardClasses["img-error"]]: hasError,
-                })}
-                src={cardNum >= 0 ? `/cards/${cardNum}.svg` : `/cards/0.svg`}
-                onLoad={handleImageLoad}
-                onError={handleImageError}
-            />
-        </Box>
-    );
-});
+        return (
+            <Box
+                {...props}
+                ref={ref}
+                sx={{
+                    ...playingCardRootSx,
+                    ...props.sx,
+                }}
+                className={clsx(
+                    playingCardClasses.root,
+                    props.classes?.root,
+                    props.className,
+                    { [playingCardClasses.back]: cardNum === -1 || hasError }
+                )}
+            >
+                <img
+                    ref={imgRef}
+                    className={clsx(
+                        playingCardClasses.img,
+                        props.classes?.img,
+                        {
+                            [playingCardClasses["img-loaded"]]: isLoaded,
+                            [playingCardClasses["img-error"]]: hasError,
+                        }
+                    )}
+                    src={
+                        cardNum >= 0 ? `/cards/${cardNum}.svg` : `/cards/0.svg`
+                    }
+                    onLoad={handleImageLoad}
+                    onError={handleImageError}
+                />
+            </Box>
+        );
+    }
+);
 
 export default PlayingCard;
