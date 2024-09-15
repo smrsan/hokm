20fda5e08e40b70300dfd4af091a9f17e03f3d9e
Mohammadreza Abolhasani <rzjoulatech@gmail.com>
2024-09-15 10:45:59 +0330

    Remove additional stuff from CardsTable

--- src/components/CardsTable.jsx
+++ src/components/CardsTable.jsx
@@ -1,27 +1,18 @@
 import { Box } from "@mui/material";
 import bgImg from "/images/table-bg.jpg";
 
-const CardsTable = ({ children }) => {
-    console.log({
-        image: bgImg,
-    });
-
-    return (
-        <Box
-            sx={{
-                width: "100vw",
-                height: "100vh",
-                top: 0,
-                left: 0,
-                position: "absolute",
-                backgroundImage: `url("${bgImg}")`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-            }}
-        >
-            {children}
-        </Box>
-    );
+/** @type {import("@mui/material").BoxProps["sx"]} */
+const rootSx = {
+    width: "100vw",
+    height: "100vh",
+    top: 0,
+    left: 0,
+    position: "absolute",
+    backgroundImage: `url("${bgImg}")`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
 };
+
+const CardsTable = ({ children }) => <Box sx={rootSx}>{children}</Box>;
 
 export default CardsTable;
