123f5a06e0c5d57eb93f1bf57deeba947a33f743
SMRSAN <5180976-smrsan1@users.noreply.gitlab.com>
2024-09-25 13:49:18 +0330

    Use VITE_BASE_URL according to vite mode

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import process from "process";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    dotenv.config({
+        path: mode === "production" ? "./.env" : `./.env.${mode}`,
+    });
+
+    console.log("Env Vars:", {
+        VITE_BASE_URL: process.env.VITE_BASE_URL,
+    });
+
+    return {
+        plugins: [react()],
+        base: process.env.VITE_BASE_URL,
+    };
+});
